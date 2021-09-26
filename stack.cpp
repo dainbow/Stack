25d5b25c533fbeb6951678d21b57c86c5a2003e3
dainbow <suslov9876@gmail.com>
2021-09-27 00:16:14 +0300

    Added header, some moves to сonditional compilation

--- stack.cpp
+++ stack.cpp
@@ -1,113 +1,4 @@
-#include <assert.h>
-#include <math.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#define LOW_LEVEL  1
-#define MID_LEVEL  2
-#define HIGH_LEVEL 3
-
-#define LOCATION __FILE__, __FUNCTION__, __LINE__
-
-#define STACK_DEBUG 3
-
-#ifndef STACK_DEBUG
-    #define STACK_DEBUG LOW_LEVEL
-#endif
-
-#define StackCtor(stack)                           \
-    Stack stack;                                   \
-    stack.creationInfo = {LOCATION, #stack};       \
-    StackCtor_(&stack);
-
-#define CheckAllStack(stack)                                                                  \
-    if (StackError error = IsAllOk(stack)) {                                                  \
-        printf("Error %s, read full description in dump file\n", ErrorToString(error));       \
-                                                                                              \
-        stack->dumpInfo = {LOCATION, #stack};                                                 \
-                                                                                              \
-        StackDump(stack);                                                                     \
-        assert(0 && "Verify failed");                                                         \
-    }
-
-typedef uint32_t canary;
-typedef uint64_t hashValue;
-typedef int32_t StackElem;
-
-const uint32_t POISON = 0xE2202;
-const canary CANARY = 0xDEADBEEF;
-const uint32_t FREE_VALUE = 0xF2EE;
-
-const uint32_t STACK_BEGINNING_CAPACITY = 50;
-const uint32_t HASH_BASE = 257;
-
-const float INCREASE_MULTIPLIER = 1.5;
-const float DECREASE_MULTIPLIER = 2;
-
-const char* NAME_OF_STACK_TYPE = "int";
-
-enum StackError {
-    NO_ERROR = 0,
-    STACK_OVERFLOW,
-    STACK_UNDERFLOW,
-    CAPACITY_NEGATIVE,
-    DATA_NULL,
-    STACK_FREE,
-    STACK_NULL,
-    CAPACITY_INFINITE,
-    LEFT_STACK_CANARY_IRRUPTION,
-    RIGHT_STACK_CANARY_IRRUPTION,
-    LEFT_DATA_CANARY_IRRUPTION,
-    RIGHT_DATA_CANARY_IRRUPTION,
-    STACK_HASH_IRRUPTION,
-    DATA_HASH_IRRUPTION
-};
-
-struct VarInfo {
-    const char* file;
-    const char* function;
-    int line;
-
-    const char* name;
-};
-
-struct Stack {
-    canary canaryLeft;
-
-    struct VarInfo creationInfo;
-    struct VarInfo dumpInfo;
-    int32_t size;
-    int32_t capacity;
-    uint8_t* data;
-
-    canary canaryRight;
-};
-
-int32_t StackCtor_(Stack* stack);
-int32_t StackDtor(Stack* stack);
-
-int32_t StackPush(Stack* stack, StackElem pushedValue);
-StackElem StackPop(Stack* stack);
-
-StackError IsStackOk(Stack* stack);
-StackError IsDataOk(Stack* stack);
-StackError IsCapacityOk(Stack* stack);
-StackError IsSizeOk(Stack* stack);
-StackError IsAllOk(Stack* stack);
-
-hashValue GetHash(uint8_t* pointer, uint32_t size);
-hashValue GetStackHash(Stack* stack);
-hashValue GetDataHash(Stack* stack);
-void WriteAllStackHash(Stack* stack);
-
-int StackDump(Stack* stack, FILE* outstream = stderr);
-const char* ErrorToString(StackError error);
-
-uint8_t* StackIncrease(Stack* stack);
-uint8_t* StackDecrease(Stack* stack);
-
-uint64_t powllu(int32_t base, int32_t power);
+#include "stack.h"
 
 int main() {
     StackCtor(intStack);
@@ -129,23 +20,26 @@ int main() {
 int32_t StackCtor_(Stack* stack) {
     assert(stack != nullptr && "STACK_NULL");
 
-    stack->canaryLeft  = CANARY;
-    stack->canaryRight = CANARY;
     stack->capacity    = STACK_BEGINNING_CAPACITY;
     stack->size        = 0;
 
-    uint32_t sizeOfData             = stack->capacity * sizeof(StackElem);
-    uint32_t sizeOfDataProtection   = 2 * (sizeof(canary) + sizeof(hashValue));
-    uint32_t sizeOfAllData          = sizeOfData + sizeOfDataProtection;
+    uint32_t sizeOfData           = stack->capacity * sizeof(StackElem);
+    uint32_t sizeOfDataProtection = 2 * (sizeof(canary) + sizeof(hashValue));
+    uint32_t sizeOfAllData        = sizeOfData + sizeOfDataProtection;
 
     stack->data = (uint8_t*)calloc(sizeOfAllData, sizeof(stack->data[0]));
     assert(stack->data != nullptr && "DATA_NULL");
 
-    uint8_t* beginningOfData = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
-    uint8_t* endOfData       = beginningOfData + sizeOfData;
+    #if (STACK_DEBUG >= MID_LEVEL)
+        stack->canaryLeft         = CANARY;
+        stack->canaryRight        = CANARY;
 
-    *(canary*)(stack->data)  = CANARY;
-    *(canary*)(endOfData)    = CANARY;
+        uint8_t* beginningOfData  = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
+        uint8_t* endOfData        = beginningOfData + sizeOfData;
+
+        *(canary*)(stack->data)   = CANARY;
+        *(canary*)(endOfData)     = CANARY;
+    #endif
 
     #if (STACK_DEBUG >= HIGH_LEVEL)
         for (int32_t curIdx = 0; curIdx < stack->capacity; curIdx++) {
@@ -222,7 +116,7 @@ StackError IsStackOk(Stack* stack) {
 
 StackError IsDataOk(Stack* stack) {
     if (stack->data == nullptr)             return  DATA_NULL;
-    if (stack->data == (uint8_t*)0xF2EE)    return STACK_FREE;
+    if (stack->data == (uint8_t*)FREE_VALUE)    return STACK_FREE;
 
     #if (STACK_DEBUG >= HIGH_LEVEL)
         hashValue dataHash = *(hashValue*)(stack->data + sizeof(canary) + sizeof(hashValue));
@@ -409,29 +303,31 @@ uint8_t* StackIncrease(Stack* stack) {
     uint32_t sizeOfDataProtection   = 2 * (sizeof(canary) + sizeof(hashValue));
     uint32_t sizeOfIncreasedData    = sizeOfData * (uint32_t)INCREASE_MULTIPLIER + 1;
 
-    uint8_t* beginningOfData        = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
-    canary* rightDataCanaryLocation = (canary*)(beginningOfData + sizeOfData);
+    #if (STACK_DEBUG >= MID_LEVEL)
+        uint8_t* beginningOfData        = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
+        canary* rightDataCanaryLocation = (canary*)(beginningOfData + sizeOfData);
 
-    canary cellCanary = *rightDataCanaryLocation;
-    *rightDataCanaryLocation = 0;
+        canary cellCanary = *rightDataCanaryLocation;
+        *rightDataCanaryLocation = 0;
+    #endif
 
     uint8_t* newPointer = (uint8_t*)realloc(stack->data, sizeOfDataProtection + sizeOfIncreasedData);
     assert(newPointer  != nullptr && "MEMORY_INCREASE_ERR");
     stack->capacity     = stack->capacity * (int32_t)INCREASE_MULTIPLIER + 1;
 
-    beginningOfData          = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
-    sizeOfData               = stack->capacity * sizeof(StackElem);
-    rightDataCanaryLocation  = (canary*)(beginningOfData + sizeOfData);
+    #if (STACK_DEBUG >= MID_LEVEL)
+        beginningOfData          = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
+        sizeOfData               = stack->capacity * sizeof(StackElem);
+
+        rightDataCanaryLocation  = (canary*)(beginningOfData + sizeOfData);
+        *rightDataCanaryLocation = cellCanary;
+    #endif
 
     #if (STACK_DEBUG >= HIGH_LEVEL)
         for (int32_t curIdx  = stack->size + 1; curIdx < stack->capacity; curIdx++) {
             *(StackElem*)(beginningOfData + curIdx*sizeof(StackElem)) = POISON;
         }
-    #endif
 
-    *rightDataCanaryLocation = cellCanary;
-
-    #if (STACK_DEBUG >= HIGH_LEVEL)
         WriteAllStackHash(stack);
     #endif
 
@@ -446,29 +342,31 @@ uint8_t* StackDecrease(Stack* stack) {
     uint32_t sizeOfDataProtection   = 2 * (sizeof(canary) + sizeof(hashValue));
     uint32_t sizeOfDecreasedData    = sizeOfData / (uint32_t)DECREASE_MULTIPLIER;
 
-    uint8_t* beginningOfData        = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
-    canary* rightDataCanaryLocation = (canary*)(beginningOfData + sizeOfData);
+    #if (STACK_DEBUG >= MID_LEVEL)
+        uint8_t* beginningOfData        = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
+        canary* rightDataCanaryLocation = (canary*)(beginningOfData + sizeOfData);
 
-    canary cellCanary   = *rightDataCanaryLocation;
-    *rightDataCanaryLocation = 0;
+        canary cellCanary   = *rightDataCanaryLocation;
+        *rightDataCanaryLocation = 0;
+    #endif
 
     uint8_t* newPointer = (uint8_t*)realloc(stack->data, sizeOfDataProtection + sizeOfDecreasedData);
     assert(newPointer  != nullptr && "MEMORY_DECREASE_ERROR");
     stack->capacity    /= (int32_t)DECREASE_MULTIPLIER;
 
-    beginningOfData          = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
-    sizeOfData               = stack->capacity * sizeof(StackElem);
-    rightDataCanaryLocation  = (canary*)(beginningOfData + sizeOfData);
+    #if (STACK_DEBUG >= MID_LEVEL)
+        beginningOfData          = stack->data + sizeof(canary) + 2 * sizeof(hashValue);
+        sizeOfData               = stack->capacity * sizeof(StackElem);
+
+        rightDataCanaryLocation  = (canary*)(beginningOfData + sizeOfData);
+        *rightDataCanaryLocation = cellCanary;
+    #endif
 
     #if (STACK_DEBUG >= HIGH_LEVEL)
         for (int32_t curIdx  = stack->size + 1; curIdx < stack->capacity; curIdx++) {
             *(StackElem*)(beginningOfData + curIdx * sizeof(StackElem)) = POISON;
         }
-    #endif
 
-    *rightDataCanaryLocation = cellCanary;
-
-    #if (STACK_DEBUG >= HIGH_LEVEL)
         WriteAllStackHash(stack);
     #endif
 
